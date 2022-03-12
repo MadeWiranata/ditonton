7971131d58dca67b88cfb7ad0bbd8fda25fb76fe
MadeWiranata <madeajhaa@gmail.com>
2022-03-13 00:13:36 +0700

    perbaikan

--- core/test/json_reader.dart
+++ core/test/json_reader.dart
@@ -5,8 +5,7 @@ String readJson(String name) {
   if (dir.endsWith('/test')) {
     dir = dir.replaceAll('/test', '');
   }
-  // ignore: unnecessary_string_escapes
-  if (dir.endsWith('/core') || dir.endsWith('\core')) {
+  if (dir.endsWith('/core') || dir.endsWith('\\core')) {
     return File('$dir/test/$name').readAsStringSync();
   }
   return File('$dir/core/test/$name').readAsStringSync();
--- tv/test/json_reader.dart
+++ tv/test/json_reader.dart
@@ -5,8 +5,7 @@ String readJson(String name) {
   if (dir.endsWith('/test')) {
     dir = dir.replaceAll('/test', '');
   }
-  // ignore: unnecessary_string_escapes
-  if (dir.endsWith('/tv') || dir.endsWith('\tv')) {
+  if (dir.endsWith('/tv') || dir.endsWith('\\tv')) {
     return File('$dir/test/$name').readAsStringSync();
   }
   return File('$dir/tv/test/$name').readAsStringSync();
